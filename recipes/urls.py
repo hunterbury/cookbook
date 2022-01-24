9039b5a66be69a7fc2934b41d3ff25a3d1f22216
hunterbury <huntersbury1@gmail.com>
2022-01-23 20:15:53 -0500

    added API views

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,23 +2,15 @@ from django.urls import include, path
 from rest_framework import routers
 from . import views
 
-router = routers.DefaultRouter()
-router.register(r'recipes', views.RecipeViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
 
 app_name = "recipes"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("add/", views.add, name="add"),
-    path("update/<str:pk>", views.update, name="update"),
-    path("delete/<str:pk>", views.delete, name="delete"),
-    path("view-recipe/<str:pk>", views.viewRecipe, name="view-recipe"),
+    # path("add/", views.add, name="add"),
+    # path("update/<str:pk>", views.update, name="update"),
+    # path("delete/<str:pk>", views.delete, name="delete"),
+    # path("view-recipe/<str:pk>", views.viewRecipe, name="view-recipe"),
     path('', include('django.contrib.auth.urls')),
     path('login/', views.loginView, name="login"),
     path('demo-login/', views.demoLogin, name="demo-login"),
--- recipes/views.py
+++ recipes/views.py
@@ -7,12 +7,49 @@ from .forms import RecipeForm, CommentForm
 from .models import Recipe, Comment
 from .filters import RecipeFilter
 from django.db.models import Q
-from rest_framework import viewsets
 from .serializers import RecipeSerializer
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.core.paginator import Paginator
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/recipe/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of recipes'
+        },
+        {
+            'Endpoint': '/recipes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single recipe object'
+        },
+        {
+            'Endpoint': '/recipes/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new recipe with data sent in post request'
+        },
+        {
+            'Endpoint': '/recipes/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing recipe with data sent in post request'
+        },
+        {
+            'Endpoint': '/recipes/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes an exiting recipe'
+        },
+    ]
+    return Response(routes)
 
 
 @api_view(['GET', 'POST', 'DELETE'])
@@ -38,121 +75,7 @@ def index(request):
         'page_obj':page_obj
     })
 
-
-class RecipeViewSet(viewsets.ModelViewSet):
-    queryset = Recipe.objects.all().order_by('date')
-    serializer_class = RecipeSerializer
-
-def index(request):
-    recipes = Recipe.objects.all().order_by('date')
-    if "recipes" not in request.session:
-        request.session["recipes"] = []
-
-    filter = RecipeFilter(request.GET, queryset=Recipe.objects.all())
-    recipes = filter.qs
-
-    # recipes = Recipe.objects.all().order_by('-date')
-    paginator = Paginator(recipes, 15)
-
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-
-
-
-    return render(request, "recipes/index.html", {
-        "recipes": recipes,
-        "filter": filter,
-        'page_obj':page_obj
-    })
-
-def loginView(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'registration/login.html', {'form': form})
-
-def demoLogin(request):
-    if request.method == 'POST':
-        username = 'demo_user'
-        password = 'demo_password'
-        user = authenticate(request, username=username, password=password)
-        login(request, user)
-        return redirect('/')
-
-def add(request):
-    filter = RecipeFilter(request.GET, queryset=Recipe.objects.all())
-
-    if request.method == "GET":
-        form = RecipeForm()
-        # ingredient_formset = IngredientFormSet()
-        # instruction_formset = InstructionFormSet()
-        return render(request, "recipes/add.html", {
-            "form":form,
-            "filter": filter
-            # "ingredient_formset":ingredient_formset,
-            # "instruction_formset": instruction_formset
-        })
-
-    elif request.method == "POST":
-        form = RecipeForm(request.POST, request.FILES)
-
-        if form.is_valid():
-            form.save()
-            recipe = form.cleaned_data.get("recipe")
-            # recipe = form.cleaned_data.get("recipe")
-            # ingredient_formset = IngredientFormSet(request.POST, instance=recipe)
-            # instruction_formset = InstructionFormSet(request.POST, instance=recipe)
-            # if ingredient_formset.is_valid() and instruction_formset.is_valid():
-            #     ingredient_formset.save() and instruction_formset.save()
-
-            request.session["recipes"] += [recipe]
-            return redirect('/')
-
-        else:    
-            return render(request, "recipes/add.html", {
-                "form": form,
-        })
-
-    return render(request, "recipes/add.html", {
-        "filter": filter
-    })
-
-def update(request, pk):
-    filter = RecipeFilter(request.GET, queryset=Recipe.objects.all())
-    recipe = Recipe.objects.get(id=pk)
-    recipeForm = RecipeForm(instance=recipe)
-
-    if request.method == "POST":
-        form = RecipeForm(request.POST, instance=recipe)
-        if form.is_valid():
-            form.save()
-            recipe = form.cleaned_data.get("recipe")
-            request.session["recipes"] += [recipe]
-            return redirect('/')
-
-    return render(request, "recipes/add.html", {
-        "form": form,
-        "filter": filter
-    })
-
-def delete(request, pk):
-    filter = RecipeFilter(request.GET, queryset=Recipe.objects.all())
-    recipe = Recipe.objects.get(id=pk)
-    if request.method == "POST":
-        recipe.delete()
-        return redirect('/')
-
-    return render(request, 'recipes/delete.html', {
-        "form": recipe,
-        "filter": filter
-    })
-
+@api_view(['GET', 'PUT', 'DELETE'])
 def viewRecipe(request, pk):
     filter = RecipeFilter(request.GET, queryset=Recipe.objects.all())
     recipe = Recipe.objects.get(id=pk)
@@ -181,3 +104,145 @@ def viewRecipe(request, pk):
         'new_comment': new_comment,
         'comment_form': comment_form
     })
+
+def loginView(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+def demoLogin(request):
+    if request.method == 'POST':
+        username = 'demo_user'
+        password = 'demo_password'
+        user = authenticate(request, username=username, password=password)
+        login(request, user)
+        return redirect('/')
+# class RecipeViewSet(viewsets.ModelViewSet):
+#     queryset = Recipe.objects.all().order_by('date')
+#     serializer_class = RecipeSerializer
+
+# def index(request):
+#     recipes = Recipe.objects.all().order_by('date')
+#     if "recipes" not in request.session:
+#         request.session["recipes"] = []
+
+#     filter = RecipeFilter(request.GET, queryset=Recipe.objects.all())
+#     recipes = filter.qs
+
+#     # recipes = Recipe.objects.all().order_by('-date')
+#     paginator = Paginator(recipes, 15)
+
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+
+
+
+#     return render(request, "recipes/index.html", {
+#         "recipes": recipes,
+#         "filter": filter,
+#         'page_obj':page_obj
+#     })
+
+# def add(request):
+#     filter = RecipeFilter(request.GET, queryset=Recipe.objects.all())
+
+#     if request.method == "GET":
+#         form = RecipeForm()
+#         # ingredient_formset = IngredientFormSet()
+#         # instruction_formset = InstructionFormSet()
+#         return render(request, "recipes/add.html", {
+#             "form":form,
+#             "filter": filter
+#             # "ingredient_formset":ingredient_formset,
+#             # "instruction_formset": instruction_formset
+#         })
+
+#     elif request.method == "POST":
+#         form = RecipeForm(request.POST, request.FILES)
+
+#         if form.is_valid():
+#             form.save()
+#             recipe = form.cleaned_data.get("recipe")
+#             # recipe = form.cleaned_data.get("recipe")
+#             # ingredient_formset = IngredientFormSet(request.POST, instance=recipe)
+#             # instruction_formset = InstructionFormSet(request.POST, instance=recipe)
+#             # if ingredient_formset.is_valid() and instruction_formset.is_valid():
+#             #     ingredient_formset.save() and instruction_formset.save()
+
+#             request.session["recipes"] += [recipe]
+#             return redirect('/')
+
+#         else:    
+#             return render(request, "recipes/add.html", {
+#                 "form": form,
+#         })
+
+#     return render(request, "recipes/add.html", {
+#         "filter": filter
+#     })
+
+# def update(request, pk):
+#     filter = RecipeFilter(request.GET, queryset=Recipe.objects.all())
+#     recipe = Recipe.objects.get(id=pk)
+#     recipeForm = RecipeForm(instance=recipe)
+
+#     if request.method == "POST":
+#         form = RecipeForm(request.POST, instance=recipe)
+#         if form.is_valid():
+#             form.save()
+#             recipe = form.cleaned_data.get("recipe")
+#             request.session["recipes"] += [recipe]
+#             return redirect('/')
+
+#     return render(request, "recipes/add.html", {
+#         "form": form,
+#         "filter": filter
+#     })
+
+# def delete(request, pk):
+#     filter = RecipeFilter(request.GET, queryset=Recipe.objects.all())
+#     recipe = Recipe.objects.get(id=pk)
+#     if request.method == "POST":
+#         recipe.delete()
+#         return redirect('/')
+
+#     return render(request, 'recipes/delete.html', {
+#         "form": recipe,
+#         "filter": filter
+#     })
+
+# def viewRecipe(request, pk):
+#     filter = RecipeFilter(request.GET, queryset=Recipe.objects.all())
+#     recipe = Recipe.objects.get(id=pk)
+
+#     comments = recipe.comments.filter(active=True)
+
+#     new_comment = None
+
+#     if request.method == 'POST':
+#         # A comment was posted
+#         comment_form = CommentForm(data=request.POST)
+#         if comment_form.is_valid():
+#             # Create Comment object but don't save to database yet          
+#             new_comment = comment_form.save(commit=False)
+#             # Assign the current post to the comment
+#             new_comment.recipe = recipe
+#             # Save the comment to the database
+#             new_comment.save()
+#     else:
+#         comment_form = CommentForm() 
+
+#     return render(request, 'recipes/view.html', {
+#         "recipe": recipe,
+#         "filter": filter,
+#         'comments': comments,
+#         'new_comment': new_comment,
+#         'comment_form': comment_form
+#     })
