632a4be63de26124b0cd45844c469eecf10141b9
hunterbury <huntersbury1@gmail.com>
2022-02-05 13:31:18 -0500

    styled buttons, added recipes

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.getRecipes, name="get-recipes"),
     path('create/', views.createRecipe, name="create-recipe"),
-    path('view/<str:pk>/', views.viewRecipe, name="view-recipe"),
-    path('delete/<str:pk>/', views.deleteRecipe, name="delete-recipe"),
-    path('update/<str:pk>/', views.updateRecipe, name="update-recipe"),
-    path('view/<str:pk>/comment/', views.createComment, name="create-comment"),
+    path('view/<slug:slug>/', views.viewRecipe, name="view-recipe"),
+    path('delete/<slug:slug>/', views.deleteRecipe, name="delete-recipe"),
+    path('update/<slug:slug>/', views.updateRecipe, name="update-recipe"),
+    path('view/<slug:slug>/comment/', views.createComment, name="create-comment"),
     path('comments/', views.getComments, name="comments")
 ]
